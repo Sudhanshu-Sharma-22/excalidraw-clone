b5e7733f7690532500b0a02792ce859eb26de252
Sudhanshu Sharma <ns5236959@gmail.com>
2026-05-10 12:23:47 +0530

    added db package and defined prisma schema & client

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,7 +3,7 @@ import z from "zod";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { authMiddleware } from "./middlewares/authMiddleware";
-import { createUserSchema } from "@repo/common/zodSchema";
+import { createUserSchema, signInSchema, createRoomSchema } from "@repo/common/zodSchema";
 
 const app = express();
 app.use(express.json());
@@ -13,7 +13,7 @@ app.post("/signup", async (req, res) => {
     const parsedData = createUserSchema.safeParse(req.body);
     if (parsedData.success) {
         const { username, password } = parsedData.data;
-        const token = jwt.sign({ username }, JWT_SECRET, { expiresIn: "1h" });
+        const token = jwt.sign({ username }, JWT_SECRET as string, { expiresIn: "1h" });
         res.json({
             token: token
         })
@@ -27,11 +27,23 @@ app.post("/signup", async (req, res) => {
 
 app.get("/signin", authMiddleware, async (req, res) => {
     // res.send("signin endpoint")
+    const parsedData = signInSchema.safeParse(req.body);
+    if (!parsedData.success) {
+        res.json({
+            message: "Incorrect inputs"
+        })
+    }
     res.redirect("/dashboard");
 })
 
 app.post("/create-room", authMiddleware, async (req, res) => {
     //db call
+    const parsedData = createRoomSchema.safeParse(req.body);
+    if (!parsedData.success) {
+        res.json({
+            message: "Incorrect inputs"
+        })
+    }
     res.json({
         roomID: "123456"
     })
